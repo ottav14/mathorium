32fe04a60a5814cafe5cdfde831c72b9c6be1cbc
ottav14 <dominicjottaviano@gmail.com>
2024-06-09 16:26:37 -0400

    Basic layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,47 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+
+function HeaderItem({ text, link }) {
+	return (
+		<div className={styles.headerItem}>
+			<a href={link}>{text}</a>
+		</div>
+	);
+}
+
+function SubjectItem({ text, link }) {
+	return (
+		<div className={styles.subjectItem}>
+			<a href={link}>{text}</a>
+		</div>
+	);
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+	<div className={styles.main}>
+		<div className={styles.header}>
+			<HeaderItem text="Home" link="/" />
+			<HeaderItem text="About" link="about" />
+			<div className={styles.headerTitle}>
+				<a href="/">Mathorium</a>
+			</div>
+			<HeaderItem text="Hire Me" link="hire" />
+			<HeaderItem text="More Projects" link="projects" />
+		</div>
+		<hr />
+		
+		<div className={styles.bodyContent}>
+			<div className={styles.subjectContainer}>
+				<div className={styles.subjectTitle}>
+					Subjects
+				</div>
+				<div className={styles.subjectItemsContainer}>
+					<SubjectItem text="Set Theory" />
+					<SubjectItem text="Linear Algebra" />
+					<SubjectItem text="Calculus" />
+				</div>
+			</div>
+		</div>
+	</div>
   );
 }
