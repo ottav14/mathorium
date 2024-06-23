7c1547a84635bfbbe1b2be06e2eb1420cc98e50a
ottav14 <dominicjottaviano@gmail.com>
2024-06-23 12:31:18 -0400

    Things starting to look pretttttyyy good.

--- app/set-theory/definitions/page.tsx
+++ app/set-theory/definitions/page.tsx
@@ -9,7 +9,28 @@ export default function Definitions() {
 			</div>
 			<div className={styles.notesContent}>
 				<Definition term="Set">
-					Hello this is the description for set.
+					A collection of objects. An object is either contained in a given set, or it is not.
+				</Definition> 
+				<Definition term="Element">
+					An object contained in a set is considered an element of said set.
+				</Definition> 
+				<Definition term="Subset">
+					A subset of a given set is a set which only contains objects that are also contained
+					within the given set. 
+				</Definition> 
+				<Definition term="Union">
+					The union of two sets, A and B, is the set comprised of all elements contained in either A or B.
+				</Definition> 
+				<Definition term="Intersection">
+					The intersection of two sets, A and B, is the set which contains only the elements which are
+					contained in both A and B. 
+				</Definition> 
+				<Definition term="Superset">
+					The superset of a given set is the set comprised of all possible subsets of the given set.
+				</Definition> 
+				<Definition term="Cartesian Product">
+					The cartesian product of two sets, A and B, is the set of all possible ordered paris (a, b),
+					where "a" is an element of A, and "b" is an element of B.
 				</Definition> 
 			</div>
 		</div>
